b03c873dc545580826b047bd5610b91765bcdf59
rssumi445 <rubinaaktersumi445@gmail.com>
2023-12-06 22:49:44 +0600

    First commit

---
+++ class5.py
@@ -0,0 +1,40 @@
+# x = 'Bangladesh'
+# print(x)
+# x = [12,'Bangladesh',32,34,'India']
+# # print(x[0],x[1],x[2])
+
+# for i in range (len(x)) : 
+    # print(x[i])
+    # print(i)
+    # print(i,x[i])
+
+
+# x.append("Apple")
+# print(x)
+# x.remove(x[1])
+# x.reverse('India')
+# print(x)
+# x.clear()
+# print(x)
+
+# x = ['12','Bangladesh','32','34','India']
+# x.sort()
+# print(x)
+# x.insert(2,19)
+# x.insert(1,"Apple")
+# print(x)
+
+# print(x.index('India'))
+
+
+# x = ['12','Bangladesh','32','34','India']
+# y = x.count('Bangladesh')
+# print(y)
+
+x = (12,23,45,657)  #Tople symble =()   #Tople a thakle kono  valu add ba remove kora jai na but list a thakle kora jai 
+# print(x)
+y = list(x)   #List symble = list(x)
+y.append('Dhaka')
+# print(y)
+x = tuple(y)
+print(x)
